945b1799a8a3652d170b9ec7d0f8a1ca30214a7f
Khanh Tran <khanhsbtqk@gmail.com>
2022-08-23 09:24:05 +0700

    first code

--- truffle-config.js
+++ truffle-config.js
@@ -2,14 +2,14 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
-    }
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
   },
   solc: {
     optimizer: {
       enabled: true,
-      runs: 200
-    }
-  }
-}
+      runs: 200,
+    },
+  },
+};
